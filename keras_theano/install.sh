c22f7be851c5e5a26d952821edce76df08aef26f
Igor Lugansky <lugansky@simicon.com>
2018-11-01 13:32:04 +0300

    try connect gpu to tf/theano - failure on Fermi gpu

--- keras_theano/install.sh
+++ keras_theano/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
-docker stop ml_keras_theano_0 && docker rm ml_keras_theano_0
+docker stop ml_keras_tf_theano_0 && docker rm ml_keras_tf_theano_0
 docker run --runtime=nvidia \
 	-v /mnt:/mnt \
 	-p 8888:8888 -p 6006:6006 \
-	--name ml_keras_theano_0 --tmpfs /tmp ml_keras_theano:latest 
+	--name ml_keras_tf_theano_0 --tmpfs /tmp ml_keras_tf_theano:latest 
